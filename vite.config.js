a150f024772c913e6540f96a9f9bed1c1e01ed6e
Lucas Di Franco Linden <lucas.linden@cwi.com.br>
2025-06-02 09:49:06 -0300

    atualizações

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,11 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    vuetify({ autoImport: true }),
-  ],
+  base: '/vue-project/',
+  plugins: [vue(), vueDevTools(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
